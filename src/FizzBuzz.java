8ace8dabc7349e801257108b4e3dd9ba9101f522
Yooham Jung <yoohamjung@Yoohamui-MacBookAir.local>
2023-09-11 20:39:48 -0400

    Task 3.3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -12,7 +12,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
@@ -35,7 +35,7 @@ class FizzBuzz {
             System.out.println(i);
 
         }
-        i +=1;
+        i += 1;
         return i;
     }
 }
